3924a4fb6a42b9038e5dd76c220d2408e4ccee93
考锡琪 <kaoxiqi@qq.com>
2025-05-17 19:31:40 +0800

    noicsp.com

---
+++ 问题 D Apple/问题 D Apple/问题 D Apple.cpp
@@ -0,0 +1,41 @@
+﻿// 问题 D Apple.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+LL x, y, n, ans;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> x >> y >> n;
+	if (x * 3 > y) {
+		if (n < 3) {
+			cout << x * n;
+			return 0;
+		}
+		else {
+			ans += n / 3 * y;
+			n -= 3 * ( n / 3 );
+			ans += n * x;
+			cout << ans;
+			return 0;
+		}
+	}
+	else {
+		cout << n * x;
+	}
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
