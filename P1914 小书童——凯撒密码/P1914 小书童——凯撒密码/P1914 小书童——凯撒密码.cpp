97df571421a7abc88918f51db38ff7fb29a6440c
考锡琪 <kaoxiqi@qq.com>
2025-07-07 10:52:03 +0800

    Pra In School

---
+++ P1914 小书童——凯撒密码/P1914 小书童——凯撒密码/P1914 小书童——凯撒密码.cpp
@@ -0,0 +1,12 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+string s;
+LL n;
+int main() {
+	cin >> n >> s;
+	for (LL i : s) {
+		cout << (char)(((i - 'a') + n) % 26 + 'a');
+	}
+	return 0;
+}
---
+++ P5733 【深基6.例1】自动修正/P5733 【深基6.例1】自动修正/P5733 【深基6.例1】自动修正.cpp
@@ -0,0 +1,14 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+string s;
+int main() {
+	cin >> s;
+	for (char i : s) {
+		if (i <= 'z' && i >= 'a') {
+			i -= 32;
+		}
+		cout << i;
+	}
+	return 0;
+}
