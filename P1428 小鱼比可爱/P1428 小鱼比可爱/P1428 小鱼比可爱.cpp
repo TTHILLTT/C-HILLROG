b83175b63e57f3cc578b0d60967808ad91f90893
考锡琪 <kaoxiqi@qq.com>
2025-07-07 09:25:47 +0800

    Pra In School

---
+++ P1428 小鱼比可爱/P1428 小鱼比可爱/P1428 小鱼比可爱.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+LL n;
+int main() {
+	cin >> n;
+	vector<LL> a(n);
+	for(LL i = 0; i < n; i++) {
+		cin >> a[i];
+		LL sum = 0;
+		for(LL j = i; j >= 0; j--) {
+			if (a[j] < a[i]) {
+				sum++;
+			}
+		}
+		cout << sum << " ";
+	}
+	return 0;
+}
