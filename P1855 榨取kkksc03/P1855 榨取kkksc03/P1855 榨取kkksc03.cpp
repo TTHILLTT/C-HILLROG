424c082dab12dce82a92bb9ec49d37af250a4d5b
考锡琪 <kaoxiqi@qq.com>
2025-07-07 14:43:31 +0800

    Pra In School

---
+++ P1855 榨取kkksc03/P1855 榨取kkksc03/P1855 榨取kkksc03.cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+LL n, M, T, dp[1005][1005];
+pair<LL, LL> p[100005];
+int main() {
+	cin >> n >> M >> T;
+	for(LL i= 1; i <= n; i++) {
+		cin >> p[i].first >> p[i].second;
+		for (LL j = M; j >= p[i].first; j--) {
+			for (LL k = T; k >= p[i].second; k--) {
+				dp[j][k] = max(dp[j][k], dp[j - p[i].first][k - p[i].second] + 1);
+			}
+		}
+	}
+	cout << dp[M][T];
+	return 0;
+}
